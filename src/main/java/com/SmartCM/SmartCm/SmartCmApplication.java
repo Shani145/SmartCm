3b75cc36d5d4ece0b6a2512b28a735a11144b171
Shani145 <chauhanshani145@gmail.com>
2024-11-17 17:24:00 +0530

    Initial commit

---
+++ src/main/java/com/SmartCM/SmartCm/SmartCmApplication.java
@@ -0,0 +1,13 @@
+package com.SmartCM.SmartCm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SmartCM/SmartCm/SmartCmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartCM.SmartCm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
