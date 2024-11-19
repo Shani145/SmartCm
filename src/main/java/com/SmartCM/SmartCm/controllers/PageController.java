470990286dbe7581a5021a597117a186c02260f5
Shani145 <chauhanshani145@gmail.com>
2024-11-19 09:24:00 +0530

    understing Fragment

--- src/main/java/com/SmartCM/SmartCm/controllers/PageController.java
+++ src/main/java/com/SmartCM/SmartCm/controllers/PageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
     @RequestMapping("/home")
-    public  String home(Model model) {
+    public String home(Model model) {
         model.addAttribute("name", "Home");
         model.addAttribute("YouTubeChannel", "Shani Chauan");
         model.addAttribute("GithubRepository", "https://github.com/");
@@ -15,5 +15,17 @@ public class PageController {
         return "home";
     }
 
+    // About Route Controller
+    @RequestMapping("/about")
+    public String about(Model model) {
+        System.out.println("Page loading");
+        return "about";
 
+    }
+    //services
+    @RequestMapping("/services")
+    public String services(Model model) {
+        System.out.println("Services Page Controller");
+        return "services";
+    }
 }
